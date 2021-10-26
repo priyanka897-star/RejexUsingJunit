232c9bd314ec3941cdfa06f0dbe6ba97bee30076
priyanka897-star <priyankamore8997@gmail.com>
2021-10-26 15:28:30 +0530

    Added:Functionality to validate firstname using Junit

--- test/java/UsermanagementJunitPrograms/UserValidatorTest.java
+++ test/java/UsermanagementJunitPrograms/UserValidatorTest.java
@@ -1,4 +1,5 @@
 package UsermanagementJunitPrograms;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -9,5 +10,19 @@ class UserValidatorTest {
 		UserValidator validator = new UserValidator();
 		boolean result = validator.validateFirstName("Priyanka");
 		Assertions.assertTrue(result);
+	}
+
+	@Test
+	public void givenFirstLetterSmall_CheckForValidation_ReturnFalse() {
+		UserValidator validator = new UserValidator();
+		boolean result = validator.validateFirstName("priya");
+		Assertions.assertFalse(result);
+	}
+
+	@Test
+	public void givenTwoLetters_CheckForValidation_ReturnFalse() {
+		UserValidator validator = new UserValidator();
+		boolean result = validator.validateFirstName("Pr");
+		Assertions.assertFalse(result);
 	}
 }
